9de442f9a1309978b8da79a4edd4acc323621bcf
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-03-07 08:15:53 +0000

    Update: all routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,43 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admins
   devise_for :members, :controllers => {
-    :registrations => 'members/registrations',
+    :registrations => "members/registrations",
   }
+
+  namespace :admins do
+    resources :articles, only: [:index, :show, :update, :destroy]
+    resources :tweets, only: [:index, :show, :update, :destroy] do
+      resources :tweet_comments, only: [:update, :destroy]
+    end
+    resources :members, only: [:index, :show, :edit, :update, :destroy]
+    resources :rooms, only: [:index, :show, :update, :destroy] do
+      resources :messages, only: [:update, :destroy]
+    end
+    resources :notices, only: [:index, :show, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :parts, only: [:index, :create, :edit, :update]
+  end
+
+  scope module: :members do
+    resources :articles do
+      resource :article_favorites, only: [:create, :destroy]
+    end
+    resources :tweets, only: [:index, :create, :show, :edit, :update, :destroy] do
+      resources :tweet_comments, only: [:create, :destroy]
+      resource :tweet_favorites, only: [:create, :destroy]
+    end
+    resources :members, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      resource :accesses, only: [:index, :create]
+      resource :blocks, only: [:index, :create, :destroy]
+      get "cancel" => "members#cancel", as: "cancel"
+      patch "cancel" => "members#withdraw", as: "withdraw"
+      get "following" => "relationships#follower", as: "following"
+      get "followers" => "relationships#followed", as: "followers"
+    end
+    resources :rooms, only: [:index, :show, :create]
+    resource :notices, only: [:new, :create]
+    get "top" => "articles#top", as: "top"
+    root 'articles#top'
+  end
 end
