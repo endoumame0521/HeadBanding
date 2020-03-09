1354ce5212ef1838c4a2171fd2e221c550bd95a3
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-03-09 17:12:11 +0000

    Updat: complete to implement the function of CRUD regarding Article(Member and Admin)

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -1,13 +1,35 @@
 class Admins::ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to admins_articles_path, notice: "記事ステータスが更新されました"
+    else
+      @article = Article.all
+      flash.now[:alert] = "#{@article.errors.count}件のエラーが有ります"
+      render "show"
+    end
   end
 
   def destroy
+    article = Article.find(params[:id])
+    if article.destroy
+      redirect_to admins_articles_path, notice: "記事が削除されました"
+    else
+      redirect_to admins_articles_path, alert: "記事の削除に失敗しました"
+    end
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:status)
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,20 @@
 class Article < ApplicationRecord
+  enum published_status: { open: true, close: false }
+  enum category: { recruit: 0, entry: 1 }
+  enum day_of_the_week: { saturday: 0, sunday: 1, both: 2, other: 3 }
+  enum band_intention: { professional: 0, indies: 1, hobby: 2 }
+  enum music_intention: { original:0, copy: 1 }
+  enum gender: { male: 0, female: 1 }
+  enum band_theme: { beginner: 0, middle: 1, senior: 2, worker: 3 }
+  enum status: { enable: true, disable: false }
+
   belongs_to :member
+  belongs_to :prefecture
   has_many :article_favorites, dependent: :destroy
   has_many :genre_articles, dependent: :destroy
   has_many :genres, through: :genre_articles
   has_many :part_articles, dependent: :destroy
   has_many :parts, through: :part_articles
+  has_many :article_cities, dependent: :destroy
+  has_many :cities, through: :article_cities
 end
