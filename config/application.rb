bbff49646ae977f0afbc3571f2219e7376f3bfa4
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-03-09 17:06:14 +0000

    Update: due to enable the 'gem 'better_errors' and gem 'binding_of_caller'

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module HeadBanding
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Osaka' #rails上での表示をタイムゾーンOSAKAに設定
+    config.active_record.default_timezone = :local #データベース保存時のタイムゾーンをOSのタイムゾーンに設定
   end
 end
