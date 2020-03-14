16d94e7a581a506ea87888566b51118dd202a0d1
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-03-14 05:57:02 +0000

    Add: function of Members

--- app/controllers/admins/members_controller.rb
+++ app/controllers/admins/members_controller.rb
@@ -1,10 +1,31 @@
 class Admins::MembersController < ApplicationController
   def index
+    @members = Member.all
   end
 
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admins_member_path(@member), notice: "会員情報を更新しました"
+    else
+      flas.now[:alert] = "#{@member.errors.count}件のエラーがあります"
+      render "edit"
+    end
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(
+      :name, :gender, :birthday, :address_prefecture, :address_city, :introduction,
+      :sound, :profile_image, :email, :status, { part_ids: [] }, { genre_ids: [] },
+      artists_attributes: [:id, :name])
   end
 end
