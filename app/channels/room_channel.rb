3e1f5551119ec2979d759c4d738db8da15355e40
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-04 13:10:55 +0000

    Update: add function of delete messages

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -11,5 +11,12 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     Message.create! body: data['message'], member_id: current_member.id, room_id: params['room']
   end
+
+  def remove(data)
+    message = Message.find(data['message_id'])
+    room_id = message.room_id
+    message.destroy!
+    ActionCable.server.broadcast "room_channel_#{room_id}", message_id: data['message_id']
+  end
 end
 
