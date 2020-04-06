baa15f566d641b0b80d20d934795943af03de9fb
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-06 13:50:04 +0000

    Fixed: sometimes cannot switch broadcast member-online <=> offline

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,18 +3,22 @@ module ApplicationCable
     identified_by :current_member
 
     def connect
-      unless env['warden'].user.nil?
-        self.current_member = find_verified_member
-        logger.add_tags 'ActionCable', current_member.name
-      end
+      self.current_member = find_verified_member
+      logger.add_tags 'ActionCable',
+                      current_member.id,
+                      current_member.name,
+                      I18n.l(current_member.online_at, format: :short)
     end
 
     protected
-
     def find_verified_member
-      verified_member = Member.find_by(id: env['warden'].user.id)
-      return reject_unauthorized_connection unless verified_member
-      verified_member
+      Member.find(session['warden.user.member.key'][0][0])
+    rescue
+      reject_unauthorized_connection
+    end
+
+    def session
+      @session ||= cookies.encrypted[Rails.application.config.session_options[:key]]
     end
   end
 end
