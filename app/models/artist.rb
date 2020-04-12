21a8af5614816631ec0fd514dedf646c3145869b
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-12 08:20:13 +0000

    Update: add custom validater of Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,19 @@ class Artist < ApplicationRecord
   belongs_to :member
 
   # バリデーションSTART------------------------------------------------------------------------------------------
-  validates :name, length: { maximum: 100 }
+  validates :name, length: { maximum: 100 }, allow_nil: true
+
+  # 空は許可、空白は禁止をするカスタムバリデーションです
+  class CheckBlankExceptEmptyValidator < ActiveModel::Validator
+    def validate(record)
+      unless record.name.empty?
+        if record.name.blank?
+          record.errors[:name] << "には文字を入力するか何も入力しないでください。"
+        end
+      end
+    end
+  end
+  # 上記のカスタムバリデーション呼び出し
+  validates_with CheckBlankExceptEmptyValidator
   # バリデーションEND--------------------------------------------------------------------------------------------
 end
