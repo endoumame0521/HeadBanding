89cd866a66d60235cd8441be0782a721ec840ae2
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-14 14:03:46 +0000

    Update: add logo fadein and fadeout on top page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,3 +85,12 @@ $(document).on('turbolinks:load', function() {
         $(form).find(":radio").filter("[data-default]").prop("checked", true);
     }
 });
+
+$(document).on('turbolinks:load', function() {
+  setTimeout(function(){
+    $('.start p').fadeIn(1600);
+  },500); //0.5秒後にロゴをフェードイン!
+  setTimeout(function(){
+    $('.start').fadeOut(500);
+  },2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
